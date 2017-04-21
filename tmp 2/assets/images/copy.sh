d0454defe8767ba780df3e7098e40ae00404392f
Sanjay <sanjaymk908@gmail.com>
2017-04-21 15:12:15 -0700

    042117 sources for Angkor Car Website for Pirun

---
+++ tmp 2/assets/images/copy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp ../../../lake.jpg lake-640x480-7.jpg
+cp ../../../pirun.jpg pirun-640x640-27.jpg
+cp ../../../van1.jpg van1-480x640-23-480x640-40.jpg
+cp ../../../van2.jpg van2-640x480-92-640x480-87.jpg
+cp ../../../van1.jpg van1-480x640-23.jpg
+cp ../../../van2.jpg van2-640x480-92.jpg
+cp ../../../angkor.jpg angkor-2004x1362-73.jpg
+
